3ee9cb8e40b8df266cbd3ca4eac5b60f114db939
Farid <155392795+Digitalturkk@users.noreply.github.com>
2024-07-23 02:10:46 +0400

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -2,15 +2,23 @@ import java.util.Scanner;
 
 public class Calculator{
     Calculator(){
-        // We need him only for declaring new objects "new Calculator()"
+        //We need him only for declaring new objects "new Calculator()"
     }
 
-    // Our methods for Calculator
+    //Our methods for Calculator
     public int add(int a, int b){return a+b;}
     public int subtract(int a, int b){return a-b;}
     public int multiply(int a, int b){return a*b;}
     public int divide(int a, int b){return a/b;}
     public int modulo(int a, int b){return a%b;}
+    public int factorial(int a){
+        int b = 1;
+        while(a != 1){
+            b *= a;
+            a--;
+        }
+        return b;
+    }
 
     public static void main(String[] args){
 
@@ -25,17 +33,18 @@ public class Calculator{
         Scanner doing = new Scanner(System.in);  // Create a Scanner object
 
         System.out.println("What you want to do?");
-        System.out.println("add, subtract, multiply, divide or modulo?");
+        System.out.println("add, subtract, multiply, divide, modulo, factorial?");
         String method = doing.nextLine(); // Read user input(str)
 
-        // Printing out what we need to calculate based on our user's choose!
-        // We may use switch here, but I prefer if-else construction
+        //Printing out what we need to calculate based on our user's choose!
+        //We may use switch here, but I prefer if-else construction
         switch (method) {
             case "add" -> System.out.println(myCalculator.add(a, b));
             case "subtract" -> System.out.println(myCalculator.subtract(a, b));
             case "multiply" -> System.out.println(myCalculator.multiply(a, b));
             case "divide" -> System.out.println(myCalculator.divide(a, b));
             case "modulo" -> System.out.println(myCalculator.modulo(a, b));
+            case "factorial" -> System.out.println(myCalculator.factorial(a));
         }
 
         System.out.print("Karabakh is Azerbaijan!");
